e6c12b95718cf5eccb000bbff29762ae0aad6cf1
Radu Tiberiu <90263623+RaduTibeiu@users.noreply.github.com>
2022-06-28 01:11:22 +0300

    updated db sql

--- sqls/db.sql
+++ sqls/db.sql
@@ -10,10 +10,6 @@ create table `user`(
 `Age` int unsigned NOT NULL,
 `Mail`varchar(255),
 `CreationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`Gamesplayed` int unsigned DEFAULT 0,
-`Wins` int unsigned DEFAULT 0,
-`Losses` int unsigned DEFAULT 0,
-`MoneyWon` int unsigned DEFAULT 0,
 `Password` varchar(255) NOT NULL,
 PRIMARY KEY (`UserId`)
 )ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb3;
